a3d0df7951c96b1b0384e922ac3a8a36c01c0d13
César Braz <126676504+CesarBraz7@users.noreply.github.com>
2023-09-02 23:15:15 -0300

    Delete tournamentProject directory

--- tournamentProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tournamentapp.urls')) #os urls da api ficam no 'localhost', logo quando o iniciamos
-]
