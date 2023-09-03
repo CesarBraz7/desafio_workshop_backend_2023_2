7efa96b985e05c77dc4360d964152f4d7e80bcdc
César Braz <126676504+CesarBraz7@users.noreply.github.com>
2023-09-02 23:15:03 -0300

    Delete tournamentapp directory

--- tournamentapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import JogadorListCreate, JogadorRetrieveUpdateDelete, NomeListCreate, NomeRetrieveUpdateDelete
-
-urlpatterns = [
-    path('nome/', NomeListCreate.as_view(), name= "create-nome"), #url para criar o nome do jogador
-    path('nome/<int:pk>/', NomeRetrieveUpdateDelete.as_view(), name = 'nome-details'),#url para editar o nome do jogador
-    path('jogadores/', JogadorListCreate.as_view(), name= "jogadores"), #url para criar as instancias do jogador
-    path('jogadores/<int:pk>/', JogadorRetrieveUpdateDelete.as_view(), name = 'artilheiros') #url para editar as instancias do jogador
-]
